4c57f9f2bab76fd91909aa7c6f5e4474f93b7038
Aymen Omri <aymenomri14.03@gmail.com>
2024-04-22 16:39:26 +0100

    Done

---
+++ src/app/Modules/product/product-details/product-details.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LightGallery } from 'lightgallery/lightgallery';
+import { ProductService } from 'src/app/Utils/Services/Product.service';
+import lgZoom from 'lightgallery/plugins/zoom'
+import { InitDetail } from 'lightgallery/lg-events';
+
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.css',
+  encapsulation: ViewEncapsulation.None
+})
+export class ProductDetailsComponent implements OnInit {
+  ngOnInit(): void {
+    this.id_product = Number(this.route.snapshot.paramMap.get('id'));
+    this.findProductById();
+  }
+
+  constructor(private route: ActivatedRoute, private productService: ProductService) { }
+
+  id_product: number = 0;
+  product: any;
+  images: any[] | undefined;
+  private lightGallery!: LightGallery;
+  private needRefresh = false;
+  selectedImage: any;
+
+  ngAfterViewChecked(): void {
+    if (this.needRefresh) {
+      this.lightGallery.refresh();
+      this.needRefresh = false;
+    }
+  }
+  settings = {
+    counter: false,
+    plugins: [lgZoom],
+  };
+
+  onInit = (detail: InitDetail): void => {
+    this.lightGallery = detail.instance;
+  };
+
+
+  findProductById() {
+    this.productService.getProductById(this.id_product).subscribe((data: any) => {
+      this.product = data;
+      this.images = this.product.images;
+      if(this.images){
+        this.selectedImage = this.images[0];
+      }
+    });
+  }
+
+  stepSize = 200;
+
+  selectImage(id: number) {
+    this.selectedImage = this.images?.find((img: any) => img.image_id === id);
+
+  }
+
+  openSelectedImage() {
+    this.lightGallery.openGallery(this.images?.indexOf(this.selectedImage));
+  }
+
+  scrollBackReviews() {
+    const container = document.querySelector('.others');
+    if (container) {
+      container.scrollLeft -= this.stepSize;
+    }
+  }
+
+  scrollForwardReviews() {
+    const container = document.querySelector('.others');
+    if (container) {
+      container.scrollLeft += this.stepSize;
+    }
+  }
+
+}
